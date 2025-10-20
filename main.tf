adb9770911812e29fd5d14bd503247088fe202fd
jelsync <jelsync@gmail.com>
2025-10-20 14:45:45 -0600

    security gruop | install server nginx

--- main.tf
+++ main.tf
@@ -36,3 +36,38 @@ resource "aws_key_pair" "nginx-server-ssh" {
     public_key = file("nginx-server.key.pub")
   
 }
+
+
+resource "aws_security_group" "nginx-server-sg" {
+  name        = "nginx-server-sg"
+  description = "Security group allowing SSH and HTTP access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "nginx-server-sg"
+    Environment = "nginx-server"
+    Owner       = "ariel.molina@caosbinario.com"
+    Team        = "DevOps"
+    Project     = "webinar"
+  }
+}
