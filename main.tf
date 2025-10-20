7f099e5e8e5ba013f59db29b6b46782732731fb1
jelsync <jelsync@gmail.com>
2025-10-20 16:09:33 -0600

    variables

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+
+variable "ami_id" {
+  description = "ID de la AMI para la instancia EC2"
+  default     = "ami-0199d4b5b8b4fde0e"
+}
+
+variable "instance_type" {
+  description = "Tipo de instancia EC2"
+  default     = "t3.micro"
+}
+
+variable "server_name" {
+  description = "Nombre del servidor web"
+  default     = "nginx-server"
+}
+
+variable "environment" {
+  description = "Ambiente de la aplicación"
+  default     = "test"
+}
+
 terraform {
   required_providers {
     aws = {
@@ -12,11 +33,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "nginx-server" {
-  ami           = "ami-0199d4b5b8b4fde0e"
-  instance_type = "t3.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
   tags = {
-    Name        = "nginx-server"
-    Environment = "nginx-server"
+    Name        = "var.server_name"
+    Environment = "var.environment"
     Owner       = "jelsync@gmail.com"
     Team        = "DevOps"
     Project     = "TESTING"
@@ -35,11 +56,11 @@ resource "aws_instance" "nginx-server" {
 }
 
 resource "aws_key_pair" "nginx-server-ssh" {
-  key_name   = "nginx-server-key"
-  public_key = file("nginx-server.key.pub")
+  key_name   = "${var.server_name}-key"
+  public_key = file("${var.server_name}.key.pub")
   tags = {
-    Name        = "nginx-server-ssh"
-    Environment = "nginx-server"
+    Name        = "${var.server_name}-ssh"
+    Environment = "${var.environment}"
     Owner       = "jelsync@gmail.com"
     Team        = "DevOps"
     Project     = "TESTING"
@@ -74,10 +95,21 @@ resource "aws_security_group" "nginx-server-sg" {
   }
 
   tags = {
-    Name        = "nginx-server-sg"
-    Environment = "nginx-server"
+    Name        = "${var.server_name}-sg"
+    Environment = "${var.environment}"
     Owner       = "jelsync@gmail.com"
     Team        = "DevOps"
     Project     = "TESTING"
   }
+}
+
+
+output "server_public_ip" {
+  description = "Dirección IP pública de la instancia EC2"
+  value       = aws_instance.nginx-server.public_ip
+}
+
+output "server_public_dns" {
+  description = "DNS público de la instancia EC2"
+  value       = aws_instance.nginx-server.public_dns
 }
